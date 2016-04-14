bf5abe99cdb5ed9b4c567d40542f081a56a842f5
Lucas BAUJARD <lubaujar@e2r2p13.42.fr>
2016-04-15 00:05:24 +0200

    Fri 12:05PM

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(express.static(__dirname + '/uploads'));
 app.use(express.static(__dirname + '/views'));
 
+app.get('/', function(req, res) {
+	res.send('index.html');
+})
+
 app.listen(port, function() {
 	console.log('Server running on port: ' + port);
 })
